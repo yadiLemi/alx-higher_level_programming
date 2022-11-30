ab142de9004682ad6291aaf54123d2e137a9f800
yadeetaabera5@gmail.com <yadetaabera5@gmail.com>
2022-11-30 14:13:52 -0800

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def to_uper(character):
         return (ord(character) - 32)
     else:
         return ord(character)
-def uppercase(string):
+  def uppercase(string):
     string_new = ""
     for character in string:
         string_new += "%c" % to_uper(character)
